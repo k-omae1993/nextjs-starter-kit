72318555fa97fa044d1b99a698b1e12c4083cd88
junk dog <omaeadmi0909@gmail.com>
2022-03-13 03:08:45 +0900

    webpack の設定完了

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 module.exports = {
   stories: [
     '../src/component/**/**/*.stories.mdx',
@@ -16,5 +18,13 @@ module.exports = {
       },
     },
   ],
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal(config) {
+    config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
+    return config;
+  },
   framework: '@storybook/react',
 };
